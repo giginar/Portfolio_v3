59d6d6d21bf0aac49732889b7cee6c32de20202d
ykucukcinar <yigitkucukcinar@hotmail.com>
2025-04-01 00:58:16 +0300

    remove some items and update gallery component

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -9,11 +9,26 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent {
+
+  selectedPhoto: any = null;
+
   photos = [
-    { url: 'assets/P9290674.JPG', title: '1' },
-    { url: 'assets/P9290681.JPG', title: '2' },
-    { url: 'assets/P9290717.JPG', title: '3' },
-    { url: 'assets/P9290873.JPG', title: '4' },
-    { url: 'assets/P9290910.JPG', title: '5' }
+    { url: 'assets/1.JPG', title: '1' },
+    { url: 'assets/2.JPG', title: '2' },
+    { url: 'assets/3.JPG', title: '3' },
+    { url: 'assets/4.JPG', title: '4' },
+    { url: 'assets/5.JPG', title: '5' },
+    { url: 'assets/6.JPG', title: '6' },
+    { url: 'assets/7.JPG', title: '7' },
+    { url: 'assets/8.JPG', title: '8' },
+    { url: 'assets/9.JPG', title: '9' }
   ];
+
+  openPhoto(photo: any): void {
+    this.selectedPhoto = photo;
+  }
+
+  closePhoto(): void {
+    this.selectedPhoto = null;
+  }
 }
